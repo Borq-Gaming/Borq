9b35004801522c383807a8a207724d6f38b5796f
Timothy <tbirrell@mastersolutions.biz>
2015-09-22 13:10:48 -0500

    outlined logic

--- app/controllers/CommandController.php
+++ app/controllers/CommandController.php
@@ -1,86 +1,36 @@
 <?php
 
-class CommandController extends \BaseController {
+class CommandController extends BaseController {
 
 	/**
 	 * Display a listing of the resource.
 	 *
 	 * @return Response
 	 */
-	public function index()
+	public function index($command)
 	{
-		//
+		$array = explode(" ", $command);
+		$base = strtolower($array[0]);
+
+		switch ($base) {
+			case 'move':
+				# code...
+				break;
+			case 'take':
+				# code...
+				break;
+			case 'hit':
+				# code...
+				break;
+			case 'eat':
+				# code...
+				break;
+			case 'use':
+				# code...
+				break;
+			default:
+				# code...
+				break;
+		}
 	}
-
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
-
 }
---
+++ app/controllers/MoveController.php
@@ -0,0 +1,17 @@
+<?php
+
+class MoveController extends BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function postDirection($direction)
+	{
+		// find current room in DB
+		// find room in specified direction
+		// check if room exisits
+		// return result
+	}
+}
