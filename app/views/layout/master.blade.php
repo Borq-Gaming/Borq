b4482fa12239be7ecdfc9c6c1d2c42e4141e995f
Matthew Reat <mreat@Matthews-MacBook-Pro.local>
2015-09-23 14:54:08 -0500

    adding cdn links for jquery and angular to master.blade.php

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -21,7 +21,8 @@
 	</main>
 
 
-<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script scr="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 @yield('script')
