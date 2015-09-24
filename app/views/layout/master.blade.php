d60b964b7f86dccd9b410d803a83ce2baec2904b
Matthew Reat <mreat@Matthews-MacBook-Pro.local>
2015-09-24 16:10:37 -0500

    adding jquery ui to master.blade

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{{ csrf_token() }}}">
-
+    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
     <link rel="stylesheet" type="text/css" href="/css/textbox.css">
@@ -16,11 +16,12 @@
 
 <body>
 
-	<main class="container">
-	@yield('content')
-	</main>
+    <main class="container">
+    @yield('content')
+    </main>
 
 
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script scr="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
