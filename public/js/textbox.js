910fe14ee11ab097c4d8a450f57fd89335ccff86
Timothy <tbirrell@mastersolutions.biz>
2015-09-24 10:13:06 -0500

    fixed connection to js problem

--- public/js/textbox.js
+++ public/js/textbox.js
@@ -1,145 +1,149 @@
 "use strict";
 
 (function() {
-    var app = angular.module("textBox", []);
+	var app = angular.module("textBox", []);
 
-    app.controller("textController", ["$log", "$http", function($log, $http) {
-        $http.get("/test").then(function(data) {
-            $log.info("Ajax request completed successfully!");
+	app.controller("textController", ["$log", "$http", function($log, $http) {
+		$(document).ready(function() {
+			$('#FakeTextbox, #Score, #PastCommands').click(function() {
+				$('#RealTextbox').focus();
+			});
+			$('#RealTextbox').keyup(function(e) {
+				var code = (e.keyCode ? e.keyCode : e.which);
+				// Enter key?
+				if(code == 13) {
+					userInput();
+					// Don't put a newline if this is the first command
+					if ($('#PastCommands').html() != '') {
+						$('#PastCommands').append('<br />');
+					}
+					$('#PastCommands').append($(this).val());
+					$(this).val('');
+					$('#FakeTextbox').text('');
+				} else {
+					$('#FakeTextbox').html($(this).val());
+				}
+			});
+			$('#RealTextbox').focus();
+		});
 
-            $log.debug(data);
-        }, function(response) {
-            $log.error("Ajax request failed for some reason!");
+		function determineCommand(command, value, valueB) {
+		if (!valueB) {
+			valueB = null;
+		}
 
-            $log.debug(response);
-        });
+			switch (command) {
+				case "move":
+				// return ajaxMove(value);
+				console.log('move');
+				break;
+				case "eat":
+				// return ajaxEat(value);
+				console.log('eat');
+				break;
+				case "hit":
+				// return ajaxHit(value);
+				console.log('hit');
+				break;
+				case "use":
+				// return ajaxUse(value, valueB);
+				console.log('use');
+				break;
+				case "take":
+				// return ajaxTake(value);
+				console.log('take');
+				break;
+				default: "command does not exit";
+			}
+		}
 
-        // $http.post("textbox.php", {
-        //     // key1: 
-        //     // key2: 
-        // }).then(function() {
-        //     $log.info("Info was sent to the server successfully!")
-        // }, function(response) {
-        //     $log.error("Ajax request failed for some reason!");
+		function userInput() {
+			console.log("testtest");
+			var input = $("#RealTextbox").val();
+			console.log(input);
+			var selectInput = input.split (' ');
+			var firstAction = selectInput[0];
+			var secondAction = selectInput[1];
+			if (selectInput[2]) {
+				determineCommand(firstAction, secondAction, selectInput[3])
+			} else {
+				determineCommand(firstAction, secondAction);
 
-        //     $log.debug(response);
-        // });
+			}
+		}
 
-        function determineCommand(command, value, valueB) {
-        if (!valueB) {
-            valueB = null;
-        }
+		function ajaxMove(value) {
+			$http.post("move/direction", {
+			user_id: id,
+			direction: value
+			
+			}).then(function() {
+				$log.info("Info was sent to the server successfully!")
+			}, function(response) {
+				$log.error("Ajax request failed for some reason!");
 
-            switch (command) {
-                case "move":
-                // return ajaxMove(value);
-                console.log('move');
-                break;
-                case "eat":
-                // return ajaxEat(value);
-                console.log('eat');
-                break;
-                case "hit":
-                // return ajaxHit(value);
-                console.log('hit');
-                break;
-                case "use":
-                // return ajaxUse(value, valueB);
-                console.log('use');
-                break;
-                case "take":
-                // return ajaxTake(value);
-                console.log('take');
-                break;
-                default: "command does not exit";
-            }
-        }
+				$log.debug(response);
+			});
+		}
 
-        function userInput() {
-            var input = $("#RealTextbox").val();
-            var selectInput = input.split (' ');
-            var firstAction = selectInput[0];
-            var secondAction = selectInput[1];
-            if (selectInput[2]) {
-                determineCommand(firstAction, secondAction, selectInput[3])
-            } else {
-                determineCommand(firstAction, secondAction);
+		function ajaxTake(value) {
+			$http.post("take/item", {
+			user_id: id,
+			take_item: value
+			
+			}).then(function() {
+				$log.info("Info was sent to the server successfully!")
+			}, function(response) {
+				$log.error("Ajax request failed for some reason!");
 
-            }
-        }
+				$log.debug(response);
+			});
+		}
 
-        function ajaxMove(value) {
-            $http.post("move/direction", {
-            user_id: id,
-            direction: value
-            
-            }).then(function() {
-                $log.info("Info was sent to the server successfully!")
-            }, function(response) {
-                $log.error("Ajax request failed for some reason!");
+		function ajaxHit(value) {
+			$http.post("hit/fight", {
+			user_id: id,
+			
+			
+			}).then(function() {
+				$log.info("Info was sent to the server successfully!")
+			}, function(response) {
+				$log.error("Ajax request failed for some reason!");
 
-                $log.debug(response);
-            });
-        }
+				$log.debug(response);
+			});
+		}
 
-        function ajaxTake(value) {
-            $http.post("take/item", {
-            user_id: id,
-            take_item: value
-            
-            }).then(function() {
-                $log.info("Info was sent to the server successfully!")
-            }, function(response) {
-                $log.error("Ajax request failed for some reason!");
+		function ajaxEat(value) {
+			$http.post("eat/item", {
+			user_id: id,
+			item: value
+			
+			}).then(function() {
+				$log.info("Info was sent to the server successfully!")
+			}, function(response) {
+				$log.error("Ajax request failed for some reason!");
 
-                $log.debug(response);
-            });
-        }
+				$log.debug(response);
+			});
+		}
 
-        function ajaxHit(value) {
-            $http.post("hit/fight", {
-            user_id: id,
-            
-            
-            }).then(function() {
-                $log.info("Info was sent to the server successfully!")
-            }, function(response) {
-                $log.error("Ajax request failed for some reason!");
+		function ajaxUse(value1, value2) {
+			$http.post("use/items", {
+			user_id: id,
+			item1: value1,
+			item2: value2
+			
+			}).then(function() {
+				$log.info("Info was sent to the server successfully!")
+			}, function(response) {
+				$log.error("Ajax request failed for some reason!");
 
-                $log.debug(response);
-            });
-        }
-
-        function ajaxEat(value) {
-            $http.post("eat/item", {
-            user_id: id,
-            item: value
-            
-            }).then(function() {
-                $log.info("Info was sent to the server successfully!")
-            }, function(response) {
-                $log.error("Ajax request failed for some reason!");
-
-                $log.debug(response);
-            });
-        }
-
-        function ajaxUse(value1, value2) {
-            $http.post("use/items", {
-            user_id: id,
-            item1: value1,
-            item2: value2
-            
-            }).then(function() {
-                $log.info("Info was sent to the server successfully!")
-            }, function(response) {
-                $log.error("Ajax request failed for some reason!");
-
-                $log.debug(response);
-            });
-        }
+				$log.debug(response);
+			});
+		}
 
 
 
-    }]);
+	}]);
 })();  
--- public/textbox.php
+++ public/textbox.php
@@ -8,13 +8,16 @@
 		position: relative;
 		left: -5000px;
 	}
-
 	#textBox {
-		width: 35%;
-		height: 35%;
+		width: 40%;
 		position: relative;
-		margin: auto;
-		top:200px;
+		margin: 25px auto;
+	}
+	#PastCommands {
+		border: red 1px solid;
+	}
+	#FakeTextbox {
+		border: black 1px solid;
 	}
 	</style>
 </head>
@@ -30,8 +33,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
 <script src="/js/textbox.js"></script>
-<script src="/js/textboxjs.js"></script>
-
 
 </body>
 </html>
