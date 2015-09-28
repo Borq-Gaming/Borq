f52ca4b27d0fb76fe9ea689e9441aeec61528403
Matthew Reat <mreat@Matthews-MacBook-Pro.local>
2015-09-28 09:10:16 -0500

    adding updated text display with rocky border.  added a couple of scroll image files for potential future

--- app/views/console.blade.php
+++ app/views/console.blade.php
@@ -6,19 +6,27 @@
 
 @section('content')
 
+<div class="container col-sm-12">
+	<div class="row">
+		<div class="col-sm-offset-1 col-sm-10 col-sm-offset-1 col-md-offset-3 col-md-6 col-md-offset-3">
+			<div id="console_outer">
+				<div id="text_wrapper">
+					<div ng-controller="textController">
 
-	<div class="col-md-6 col-md-offset-3" id='console'>
-	
-		<div ng-controller="textController">
-
-			<div id="textBox">
-			
-				<div id="PastCommands"></div>
-					> <span id="FakeTextbox"></span><span id="Score">_</span>
-						<input type="text" id="RealTextbox" />
+						<div id="textBox">
+						
+							<div id="PastCommands"></div>
+								> <span id="FakeTextbox"></span><span id="Score">_</span>
+									<input type="text" id="RealTextbox"/>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
+</div>
+
+
 <!-- <div class="row">
 <button type="button" class="btn btn-lg btn-primary" disabled="disabled" id="health">Health</button>
 <button type="button" class="btn btn-lg btn-primary" disabled="disabled" id="items">Items</button>
