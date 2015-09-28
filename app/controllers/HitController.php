978114cc2a7478491d4b78799da3a6f6b8d24511
Timothy <tbirrell@mastersolutions.biz>
2015-09-28 15:49:51 -0500

    started on hit controller

--- app/controllers/HitController.php
+++ app/controllers/HitController.php
@@ -7,15 +7,55 @@ class HitController extends BaseController {
 	 *
 	 * @return Response
 	 */
-	public function getFight()
+
+	//var bank
+	public $sword;
+	public $armor;
+	public $hp;
+
+	public function postGaurd() 
 	{
+		$this->fetchStats();
+		$this->fetchGaurd();
+	}
+
+	// fetch all necessary stats and set up
+	public function fetchStats()
+	{
+		$stats = Auth::user();
+		
 		// fetch sword and armor stats
+		$sword = $stats->sword;
+		$armor = $stats->armor;
+		
 		// fetch HP
+		$this->hp = $stats->health;
+
+		if ($sword) {
+			$this->sword = 1.15;
+		} else {
+			$this->sword = 1;
+		}
+
+		if ($armor) {
+			$this->armor = 1.15;
+		} else {
+			$this->armor = 1;
+		}
+	}
+
+	public function fetchGuard()
+	{
+		$game = Auth::user();
+		$location = $game->player_location_id;
+		$player = $game->id;
+
+	}
+	
 		// RNG for hit/miss
 		// if hit, simulate hit
 		// RNG for counter attack hit/miss
 		// if hit, simulate hit
 		// save new HP to DB
 		// return results
-	}
 }
