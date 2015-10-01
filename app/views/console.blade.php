dcd4387004a27c0568c2629a48ef964fad3e2c90
David <d_rod3663@hotmail.com>
2015-10-01 14:42:06 -0500

    merge needed for pull

--- app/views/console.blade.php
+++ app/views/console.blade.php
@@ -10,27 +10,23 @@
 <div id="borq_image">
 	<img src="/images/Borq.png" alt="borq logo"/>
 </div>
-<img src="/images/location.png"/>
-<img src="/images/health.png"/>
-<img src="/images/inventory.png"/>
 
-<!-- <div id="instructions">
+<!-- <img src="/images/location.png"/>
+<img src="/images/health.png"/>
+<img src="/images/inventory.png"/> -->
+
+<div id="instructions">
 	<a href="" span style="cursor: pointer" id="myModal2">
 	<img src="/images/instructions.png">
 	</a></span>
-</div> -->
-
-<!-- <div id="instructions">
-	<a class="image-popup-no-margins" href="/images/scroll1 copy.png">
-		<img src="/images/scroll-paper-parchment-border-background-leather copy.png" width="250" height="75">
-	</a>
-</div> -->
-
-<div id="instructions">
-	<a class="image-popup-no-margins" href="/images/kc-old-scroll-md1 copy.png">
-		<img src="/images/scroll-paper-parchment-border-background-leather copy.png" width="250" height="75">
-	</a>
 </div>
+
+<!-- REVISED INSTRUCTION MODAL AS AN IMAGE -->
+<!-- <div id="instructions">
+	<a class="image-popup-no-margins" href="/images/kc-old-scroll-md1 copy.png">
+		<img src="/images/instructions.png" width="250" height="75">
+	</a>
+</div> -->
 
 
 <div class="container col-sm-12" id="game_box" style="display: none;">
@@ -39,11 +35,13 @@
 
 			<div class="row">
 				<div id="location" class="col-sm-6">
+					<!-- <img src="/images/location.png"/> -->
 					<label class="label" id="location_label" for="location">Location</label></br>
 					<input id="current_location" disabled>
 				</div>
 
 				<div id="health" class="col-sm-6">
+					<!-- <img src="/images/health.png"/> -->
 					<label class="label" for="health">Health</label>
 					<div id="health_bar"></div>
 				</div>
@@ -53,6 +51,7 @@
 			<div class='row'>
 				<div class='form-group col-sm-12'>
 					<div name="items" for="items">
+						<!-- <img src="/images/inventory.png"/> -->
 						<label class="label" class="form-group" id="item_label" name="items">Inventory</label>
 						<div id='items'></div>
 					</div>
@@ -87,7 +86,7 @@
 	
 
 <div style="display: none;" id="grabMe">
-	<strong>Hit:</strong> When you encounter a guard and wish to engage simply type the command "hit" <br><br>
+	<strong>Hit:</strong> When you encounter a guard and wish to engage simply type the command "hit guard" <br><br>
 	<strong>Use:</strong> When you have an item is available for use type command "use + the item name"<br><br>
 	<strong>Eat:</strong> When food is available in your items type command "eat + the item name"<br><br>
 	<strong>Move:</strong> To make your way through the castle type command "move +"<br>
@@ -104,7 +103,7 @@
 <script>
 
 $(document).ready(function() {
-
+	// REVISED INSTRUCTION MODAL AS AN IMAGE
 	$('.image-popup-no-margins').magnificPopup({
 		type: 'image',
 		closeOnContentClick: true,
